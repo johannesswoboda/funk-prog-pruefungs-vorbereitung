2cf77649445b2c76ffb30e0d3f2689bd20143a8e
Johannes Swoboda <e1110380@student.tuwien.ac.at>
2018-04-26 11:35:53 +0200

    ident zu 2018-03-02

---
+++ prüfung-2017-01-19/Aufgabe2.hs
@@ -0,0 +1,18 @@
+-- Ident zu Aufgabe 2 von 2018-03-02
+
+{- Aufgabe 2 (5%)
+Definieren Sie einen polymorphen algebraischen Datentyp BTree zur Darstellung von binären Bäumen. Knoten enthalten neben einem polymorphen Element zwei Unterbäume. Weiters gibt es auch leere Bäume und leere Unterbäume.
+-}
+
+{- Aufgabe 3 (5%)
+Geben Sie einen konkreten Wert des Typs BTree (BTree Integer, String), der die Werte 1, 2 und "ab" enthält, in Haskell- Syntax und als Zeichnung an.
+-}
+
+{- Aufgabe 4 (5+15%)
+Definieren Sie juniq, das für einen Baum des Typs BTree genau dann True ist, wenn in jedem Knoten das polymorphe Element höchstens einmal in den Unterbäumen des Knotens vorkommt. Geben Sie für juniq auch die entsprechende Typdeklaration an. (Typklasse!)
+-}
+
+{- Definieren Sie trx, das zwei Argumente erwartet: Eine einstellige boolsche Funktion und einen BTree. Die Funktion liefere eine Liste aus Tripeln jener Knoten zurück, für die die Funktion zutrifft (angewandt auf den gesamten Knoten). Im Tripel ist das erste Element das Knotenelement, das zweite der linke und das dritte der rechte Unterbaum. Die Elemente der Liste kommen in beliebiger Reihenfolge vor. Geben Sie auch hier eine möglichst allgemeine Deklaration an!
+-}
+
+
